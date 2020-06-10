320448b75620c026bcbcabff08e8926bb813e017
Oscar Islas <oislasreyes@gmail.com>
2020-06-10 13:35:05 -0500

    Configurando el proxy y hacíendo llamada a /csrf para obtener token

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import { Chart } from 'react-chartjs-2';
+import { ThemeProvider } from '@material-ui/styles';
+import validate from 'validate.js';
+
+import { chartjs } from './helpers';
+import theme from './theme';
+import 'react-perfect-scrollbar/dist/css/styles.css';
+import './assets/scss/index.scss';
+import validators from './common/validators';
+import Routes from './Routes';
+
+const browserHistory = createBrowserHistory();
+
+Chart.helpers.extend(Chart.elements.Rectangle.prototype, {
+  draw: chartjs.draw
+});
+
+validate.validators = {
+  ...validate.validators,
+  ...validators
+};
+
+export default class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      token: ''
+    }
+  }
+
+  // componentDidMount(){
+  //   fetch('/csrf').then( response => {
+  //     return response.json();
+  //   }).then( results => {      
+  //     this.setState({token: results.csrfToken});
+  //   })    
+  // }
+
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <Router history={browserHistory}>
+          <Routes token={this.state.token} />
+        </Router>
+      </ThemeProvider>
+    );
+  }
+}
---
+++ src/views/SignIn/SignIn.js
@@ -0,0 +1,345 @@
+import React, { useState, useEffect } from 'react';
+import { Link as RouterLink, withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import validate from 'validate.js';
+import { makeStyles } from '@material-ui/styles';
+import {
+  Grid,
+  Button,
+  IconButton,
+  TextField,
+  Link,
+  Typography
+} from '@material-ui/core';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+
+import { Facebook as FacebookIcon, Google as GoogleIcon } from 'icons';
+
+const schema = {
+  email: {
+    presence: { allowEmpty: false, message: 'is required' },
+    email: true,
+    length: {
+      maximum: 64
+    }
+  },
+  password: {
+    presence: { allowEmpty: false, message: 'is required' },
+    length: {
+      maximum: 128
+    }
+  }
+};
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    backgroundColor: theme.palette.background.default,
+    height: '100%'
+  },
+  grid: {
+    height: '100%'
+  },
+  quoteContainer: {
+    [theme.breakpoints.down('md')]: {
+      display: 'none'
+    }
+  },
+  quote: {
+    backgroundColor: theme.palette.neutral,
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundImage: 'url(/images/auth.jpg)',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center'
+  },
+  quoteInner: {
+    textAlign: 'center',
+    flexBasis: '600px'
+  },
+  quoteText: {
+    color: theme.palette.white,
+    fontWeight: 300
+  },
+  name: {
+    marginTop: theme.spacing(3),
+    color: theme.palette.white
+  },
+  bio: {
+    color: theme.palette.white
+  },
+  contentContainer: {},
+  content: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  contentHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    paddingTop: theme.spacing(5),
+    paddingBototm: theme.spacing(2),
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2)
+  },
+  logoImage: {
+    marginLeft: theme.spacing(4)
+  },
+  contentBody: {
+    flexGrow: 1,
+    display: 'flex',
+    alignItems: 'center',
+    [theme.breakpoints.down('md')]: {
+      justifyContent: 'center'
+    }
+  },
+  form: {
+    paddingLeft: 100,
+    paddingRight: 100,
+    paddingBottom: 125,
+    flexBasis: 700,
+    [theme.breakpoints.down('sm')]: {
+      paddingLeft: theme.spacing(2),
+      paddingRight: theme.spacing(2)
+    }
+  },
+  title: {
+    marginTop: theme.spacing(3)
+  },
+  socialButtons: {
+    marginTop: theme.spacing(3)
+  },
+  socialIcon: {
+    marginRight: theme.spacing(1)
+  },
+  sugestion: {
+    marginTop: theme.spacing(2)
+  },
+  textField: {
+    marginTop: theme.spacing(2)
+  },
+  signInButton: {
+    margin: theme.spacing(2, 0)
+  }
+}));
+
+const SignIn = props => {
+  const { history } = props;
+
+  const classes = useStyles();
+
+  const [token, setToken] = useState();
+
+  const [formState, setFormState] = useState({
+    isValid: false,
+    values: {},
+    touched: {},
+    errors: {}
+  });
+
+  //Para obtener el token csrf y enviarlo en las peticiones POST, PUT y DELETE
+  useEffect( () => {
+    fetch('/csrf').then( response => {
+      return response.json();
+    }).then( results => {      
+      //Colocar el token en el estado
+      setToken(results.csrfToken);
+    });
+  }, []);
+
+  //1. Implementar la función para el inicio de sesión
+  /* haciendo uso de una petición a http://localhost/login
+  *  A través del protocolo HTTP por el método POST
+  *  Y enviar los datos de email y password en el body de la petición. Para más información
+  * consultar https://developer.mozilla.org/es/docs/Web/API/Fetch_API/Utilizando_Fetch
+  */ 
+  const login = () => {
+    fetch('/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json', 'XSRF-Token': token },
+      credentials: 'include',
+      body: JSON.stringify(formState.values)
+    }).then( response => {      
+      return response.json();
+    }).then( results => {
+      // localStorage.setItem('tkn', results.token);
+      console.log(results);      
+    }).catch( error => console.log(error));
+  }
+
+  useEffect(() => {
+    const errors = validate(formState.values, schema);
+
+    setFormState(formState => ({
+      ...formState,
+      isValid: errors ? false : true,
+      errors: errors || {}
+    }));
+  }, [formState.values]);
+
+  const handleBack = () => {
+    history.goBack();
+  };
+
+  const handleChange = event => {
+    event.persist();
+
+    setFormState(formState => ({
+      ...formState,
+      values: {
+        ...formState.values,
+        [event.target.name]:
+          event.target.type === 'checkbox'
+            ? event.target.checked
+            : event.target.value
+      },
+      touched: {
+        ...formState.touched,
+        [event.target.name]: true
+      }
+    }));
+  };
+
+  const handleSignIn = event => {
+    event.preventDefault();
+    history.push('/');
+  };
+
+  const hasError = field =>
+    formState.touched[field] && formState.errors[field] ? true : false;
+
+  return (
+    <div className={classes.root}>
+      <Grid
+        className={classes.grid}
+        container
+      >
+        <Grid
+          className={classes.quoteContainer}
+          item
+          lg={5}
+        >
+          <div className={classes.quote}>
+            <div className={classes.quoteInner}>
+              <Typography
+                className={classes.quoteText}
+                variant="h1"
+              >
+                Hella narwhal Cosby sweater McSweeney's, salvia kitsch before
+                they sold out High Life.
+              </Typography>
+              <div className={classes.person}>
+                <Typography
+                  className={classes.name}
+                  variant="body1"
+                >
+                  Takamaru Ayako
+                </Typography>
+                <Typography
+                  className={classes.bio}
+                  variant="body2"
+                >
+                  Manager at inVision
+                </Typography>
+              </div>
+            </div>
+          </div>
+        </Grid>
+        <Grid
+          className={classes.content}
+          item
+          lg={7}
+          xs={12}
+        >
+          <div className={classes.content}>
+            <div className={classes.contentHeader}>
+              <IconButton onClick={handleBack}>
+                <ArrowBackIcon />
+              </IconButton>
+            </div>
+            <div className={classes.contentBody}>
+              <form
+                className={classes.form}
+                onSubmit={handleSignIn}
+              >
+                <Typography
+                  className={classes.title}
+                  variant="h2"
+                >
+                  Sign in
+                </Typography>
+                <Typography
+                  color="textSecondary"
+                  gutterBottom
+                >
+                  Sign in with social media
+                </Typography>                               
+                <TextField
+                  className={classes.textField}
+                  error={hasError('email')}
+                  fullWidth
+                  helperText={
+                    hasError('email') ? formState.errors.email[0] : null
+                  }
+                  label="Email address"
+                  name="email"
+                  onChange={handleChange}
+                  type="text"
+                  value={formState.values.email || ''}
+                  variant="outlined"
+                />
+                <TextField
+                  className={classes.textField}
+                  error={hasError('password')}
+                  fullWidth
+                  helperText={
+                    hasError('password') ? formState.errors.password[0] : null
+                  }
+                  label="Password"
+                  name="password"
+                  onChange={handleChange}
+                  type="password"
+                  value={formState.values.password || ''}
+                  variant="outlined"
+                />
+                <Button
+                  className={classes.signInButton}
+                  color="primary"
+                  disabled={!formState.isValid}
+                  fullWidth
+                  size="large"
+                  type="submit"
+                  variant="contained"
+                  onClick={login}
+                >
+                  Sign in now
+                </Button>
+                <Typography
+                  color="textSecondary"
+                  variant="body1"
+                >
+                  Don't have an account?{' '}
+                  <Link
+                    component={RouterLink}
+                    to="/sign-up"
+                    variant="h6"
+                  >
+                    Sign up
+                  </Link>
+                </Typography>
+              </form>
+            </div>
+          </div>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+SignIn.propTypes = {
+  history: PropTypes.object
+};
+
+export default withRouter(SignIn);
